95c7cfc91e245479373743b48313d7ac8dd9b913
Boby Nugraha <bobynugraha19@gmail.com>
2024-01-26 19:33:36 +0700

    set config database

--- config/config.js
+++ config/config.js
@@ -19,8 +19,14 @@ module.exports = {
     password: process.env.PRODUCTION_DB_PASSWORD,
     database: process.env.PRODUCTION_DB_NAME,
     host: process.env.PRODUCTION_DB_HOST,
-    port: process.env.PRODUCTION_DB_PORT,
-    dialect: 'mysql'
+    // port: process.env.PRODUCTION_DB_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Nonaktifkan penolakan koneksi jika sertifikat SSL tidak valid
+      }
+    }
   },
   // ...
 };
